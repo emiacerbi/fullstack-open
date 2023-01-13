5b5c8a15776b30c557542056f8399c7bc5c901c2
Emiliano Acerbi <eacerbip@gmail.com>
2023-01-13 18:51:49 -0300

    first exercise

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,26 @@
+import Content from './components/Content'
+import Header from './components/Header'
+import Total from './components/Totat'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const parts = [part1, part2, part3]
+  const exercises = [exercises1, exercises2, exercises3]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
