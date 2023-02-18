e77957c9ec40d6dd53e97380ac3d144bb78700ab
Emiliano Acerbi <eacerbip@gmail.com>
2023-02-18 20:22:46 -0300

    exercises 9.6 - 9.7

--- part9/typescript-module/exerciseCalculator.ts
+++ part9/typescript-module/exerciseCalculator.ts
@@ -1,36 +1,36 @@
 export const calculateExercise = (array: number[], target: number) => {
-  if (isNaN(target)) throw new Error('Target must be a number')
+  if (isNaN(target)) throw new Error('Target must be a number');
 
   array.forEach((element) => {
     if (isNaN(element)) {
-      throw new Error('Array must have all numbers')
+      throw new Error('Array must have all numbers');
     }
-  })
+  });
 
-  const periodLength = array.length
+  const periodLength = array.length;
   const trainingDays = array.reduce((acc, el) => {
-    if (el > 0) acc += 1
-    return acc
-  }, 0)
+    if (el > 0) acc += 1;
+    return acc;
+  }, 0);
 
-  const totalHoursNeeded = array.length * target
-  const totalHoursTrained = array.reduce((acc, el) => (acc += el))
-  const success = totalHoursTrained >= totalHoursNeeded
+  const totalHoursNeeded = array.length * target;
+  const totalHoursTrained = array.reduce((acc, el) => (acc += el));
+  const success = totalHoursTrained >= totalHoursNeeded;
 
   const rating =
     totalHoursTrained < totalHoursNeeded / 2
       ? 1
       : totalHoursTrained < totalHoursNeeded
       ? 2
-      : 3
+      : 3;
 
   const ratingDescriptionObj = {
     1: 'could be better',
     2: 'pretty good',
     3: 'perfect score',
-  }
+  };
 
-  const average = totalHoursTrained / periodLength
+  const average = totalHoursTrained / periodLength;
 
   return {
     periodLength,
@@ -40,20 +40,20 @@ export const calculateExercise = (array: number[], target: number) => {
     ratingDescription: ratingDescriptionObj[rating],
     target,
     average,
-  }
-}
+  };
+};
 
-const target = Number(process.argv[2])
-const array = [...process.argv].slice(3).map((el) => Number(el))
+const target = Number(process.argv[2]);
+const array = [...process.argv].slice(3).map((el) => Number(el));
 
 try {
-  console.log(calculateExercise(array, target))
+  console.log(calculateExercise(array, target));
 } catch (error) {
-  let errorMessage = 'Something went wrong: '
+  let errorMessage = 'Something went wrong: ';
 
   if (error instanceof Error) {
-    errorMessage += error.message
+    errorMessage += error.message;
   }
 
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
--- part9/typescript-module/index.ts
+++ part9/typescript-module/index.ts
@@ -1,30 +1,58 @@
-import express from 'express'
-import { calculateBmi } from './bmiCalculator'
-const app = express()
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+// import { calculateExercise } from './exerciseCalculator';
+const app = express();
+import bp from 'body-parser';
+import { calculateExercise } from './exerciseCalculator';
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 app.get('/ping', (_req, res) => {
-  res.send('pong')
-})
+  res.send('pong');
+});
 
 app.get('/bmi', (req, res) => {
   try {
-    const weight = Number(req.query.weight)
-    const height = Number(req.query.height)
-    const result = calculateBmi(height, weight)
-    res.send(result)
+    const weight = Number(req.query.weight);
+    const height = Number(req.query.height);
+    const result = calculateBmi(height, weight);
+    res.send(result);
   } catch (error) {
-    let errorMessage = 'malformatted parameters: '
+    let errorMessage = 'malformatted parameters: ';
 
     if (error instanceof Error) {
-      errorMessage += error.message
+      errorMessage += error.message;
     }
 
-    res.json({ error: errorMessage })
+    res.json({ error: errorMessage });
   }
-})
+});
 
-const PORT = 3003
+app.post('/exercises', (req, res) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { daily_exercises, target } = req.body;
+
+    const result = calculateExercise(
+      daily_exercises as number[],
+      target as number
+    );
+
+    res.send(result);
+  } catch (error) {
+    let errorMessage = 'malformatted parameters: ';
+
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+
+    res.json({ error: errorMessage });
+  }
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
